13d2623ff8a2e1d8fc3f059346fe5734ce1f45f3
= <=>
2022-03-04 19:18:38 -0700

    working ong views

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+const path = require("path");
+
 const express = require("express");
 const bodyParser = require("body-parser");
 
@@ -6,6 +8,16 @@ const app = express();
 app.set("view engine", "ejs");
 app.set("views", "views");
 
+// import route files
+const adminRoutes = require("./routes/admin");
+const songsRoutes = require("./routes/songs");
+const authRoutes = require("./routes/auth");
+
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, "public")));
+
+app.use("/admin", adminRoutes);
+app.use(songsRoutes);
+app.use(authRoutes);
 
 app.listen(5050);
