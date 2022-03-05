c1bdca988ebe9d02fa40117e3a934d436c884710
= <=>
2022-03-04 18:41:00 -0700

    setting up app.js

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 
 const app = express();
 
-app.listen(5050);
+app.set("view engine", "ejs");
+app.set("views", "views");
 
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.listen(5050);
